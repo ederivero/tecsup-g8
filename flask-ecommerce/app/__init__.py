dfd8ae610857694ab3ecbad772537dfaec63fa76
Paolo <paholo2054@gmail.com>
2022-08-26 21:34:43 -0500

    26/08 cambio de base de datos en flask

--- flask-ecommerce/app/__init__.py
+++ flask-ecommerce/app/__init__.py
@@ -21,7 +21,8 @@ basicConfig(
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = "super-secret"
 
